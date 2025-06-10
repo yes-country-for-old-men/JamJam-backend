297861fded0992d1f0ea50ecbc86403da9babc87
이상윤 <leesy010504@gmail.com>
2025-06-10 15:55:46 +0900

    chore: 프로젝트 초기 설정 >> >> - ref: #1

---
+++ src/test/java/com/jamjam/JamjamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamjam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamjamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
