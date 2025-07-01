305dee371525ac8e348816b954242e0c81973770
kokeunho <kokangsik65@naver.com>
2025-07-01 17:50:53 +0900

    fix: S3Config local, prob 코드 개별적으로 작성

---
+++ src/main/java/com/jamjam/global/config/S3ProdConfig.java
@@ -0,0 +1,24 @@
+package com.jamjam.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@Profile("prod")
+public class S3ProdConfig {
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
