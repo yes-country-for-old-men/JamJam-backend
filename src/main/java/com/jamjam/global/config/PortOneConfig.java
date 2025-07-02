2f744e9c9454c80d0a576ddd3343fd2b0e387728
kokeunho <kokangsik65@naver.com>
2025-07-02 17:16:32 +0900

    chore: portone 연동 설정

---
+++ src/main/java/com/jamjam/global/config/PortOneConfig.java
@@ -0,0 +1,19 @@
+package com.jamjam.global.config;
+
+import com.siot.IamportRestClient.IamportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PortOneConfig {
+    @Value("${spring.portone.api-key")
+    private String apiKey;
+    @Value("${spring.portone.secret-key")
+    private String secretKey;
+
+    @Bean
+    public IamportClient iamportClient() {
+        return new IamportClient(apiKey, secretKey);
+    }
+}
