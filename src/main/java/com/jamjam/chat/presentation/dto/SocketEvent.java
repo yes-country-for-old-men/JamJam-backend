6da39713a9f107ac4d23b42076c057b8d6018761
leesy010504 <148897132+leesy010504@users.noreply.github.com>
2025-07-31 01:27:44 +0900

    Update SocketEvent.java

--- src/main/java/com/jamjam/chat/presentation/dto/SocketEvent.java
+++ src/main/java/com/jamjam/chat/presentation/dto/SocketEvent.java
@@ -3,7 +3,7 @@ package com.jamjam.chat.presentation.dto;
 import com.jamjam.chat.domain.entity.SocketEventType;
 
 public record SocketEvent<T> (
-        SocketEventType type,
+        SocketEventType type, // test
         T content
 ) {
 }
