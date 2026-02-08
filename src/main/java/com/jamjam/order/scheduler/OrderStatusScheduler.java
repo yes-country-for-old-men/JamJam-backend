ea73f127269ccf4591120cbe36e7be7f7ab01008
kokeunho <kokangsik65@naver.com>
2026-02-09 04:21:56 +0900

    fix: OrderStatusScheduler transactional 추가

--- src/main/java/com/jamjam/order/scheduler/OrderStatusScheduler.java
+++ src/main/java/com/jamjam/order/scheduler/OrderStatusScheduler.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -23,6 +24,7 @@ public class OrderStatusScheduler {
     private final NotificationSender notificationSender;
 
     @Scheduled(cron = "0 0 * * * *")
+    @Transactional
     public void autoCompleteOrders() {
         LocalDateTime threeDaysAgo = LocalDateTime.now().minusDays(3);
         List<OrderEntity> orders = orderRepository.findWaitingConfirmOrdersBefore(threeDaysAgo);
